cc4940f29fa96a8051b3594c1d8a44da67687d90
Дмитрий Кассин <kassindima1@gmail.com>
2022-12-04 08:42:47 +0200

    ***

---
+++ Nojiki/Nojiki/wwwroot/Script.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener('DOMContentLoaded', (e) => {
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let urlApi = 'https://localhost:7077/Knives';
+        fetch(urlApi)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                let container = document.getElementById('container');
+                for (const iterator of data) {
+                    let cart = document.createElement('div');
+                    cart.setAttribute('class', 'cart');
+                    cart.setAttribute('id', `${iterator['Name']}_${iterator['Steel']}`);
+                    cart.innerText = `${iterator['Name']}-${iterator['Steel']}: ${iterator['Price']} / ${iterator['sale']}`;
+                    container.append(cart);
+                }
+                // console.log(data);
+            })
+            .catch((err) => {
+                console.error("BAD API!");
+            });
+    });
+});
